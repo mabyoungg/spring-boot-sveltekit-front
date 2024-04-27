dd8927eb620ce790de7b0aa8563e832968ee09bb
mabyoungg <mabyoungg@gmail.com>
2024-04-27 19:52:51 +0900

    refactor: add input

--- src/routes/test1/+page.svelte
+++ src/routes/test1/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	const username = 'mabyoungg';
+	let username = $state('');
 
 	let repositories = $state([]);
 
-	$effect(async () => {
+	async function fetchRepositories() {
 		const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=10`);
 		repositories = await response.json();
-	});
+	}
 </script>
 
 <svelte:head>
@@ -14,11 +14,20 @@
 	<meta name="description" content="test 화면" />
 </svelte:head>
 
-<h1>{username}'s repositories</h1>
-<ul>
-	{#each repositories as repo}
-		<li>
-			{repo.name}
-		</li>
-	{/each}
-</ul>
+<div class="p-5">
+	<form on:submit|preventDefault={fetchRepositories}>
+		<input type="text" bind:value={username} placeholder="GitHub 사용자 이름 입력"/>
+		<button type="submit">데이터 가져오기</button>
+	</form>
+
+	{#if username}
+		<h1>{username}'s repositories</h1>
+		<ul>
+			{#each repositories as repo}
+				<li>
+					- {repo.name}
+				</li>
+			{/each}
+		</ul>
+	{/if}
+</div>
