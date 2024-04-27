7f34b57d9fe92b59ffaa4e36bef31430bfa6d6b1
mabyoungg <mabyoungg@gmail.com>
2024-04-27 19:48:55 +0900

    feat: github repository load page

---
+++ src/routes/test1/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	const username = 'mabyoungg';
+
+	let repositories = $state([]);
+
+	$effect(async () => {
+		const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=10`);
+		repositories = await response.json();
+	});
+</script>
+
+<svelte:head>
+	<title>test</title>
+	<meta name="description" content="test 화면" />
+</svelte:head>
+
+<h1>{username}'s repositories</h1>
+<ul>
+	{#each repositories as repo}
+		<li>
+			{repo.name}
+		</li>
+	{/each}
+</ul>
