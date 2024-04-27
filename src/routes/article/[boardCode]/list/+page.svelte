c2157aa7a9a2417daaf18920e097e4d95c61e584
mabyoungg <mabyoungg@gmail.com>
2024-04-27 22:14:25 +0900

    feat: 동적 라우팅 테스트

---
+++ src/routes/article/[boardCode]/list/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import {page} from '$app/stores';
+
+	const boardCode = $page.params['boardCode'];
+
+	const boardNames = {
+		notice: '공지',
+		free1: '자유1',
+		free2: '자유2'
+	};
+
+	const boardName = ((boardNames[boardCode] || '') + ' 게시판').trim();
+</script>
+
+<h1>{boardName}</h1>
