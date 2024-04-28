ebab2d832bc475e1c62af3d06049592c1e8ae3f1
mabyoungg <mabyoungg@gmail.com>
2024-04-28 21:46:08 +0900

    feat: 서버 통신 테스트

---
+++ src/routes/article/list/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	let articles = $state([]);
+	$effect(() => {
+		fetch('http://localhost:8090/api/v1/articles')
+			.then(response => response.json())
+			.then(json => articles = json);
+	});
+</script>
+
+<div>
+	<h1>Articles</h1>
+
+	{JSON.stringify(articles)}
+	<hr>
+	<ul>
+		{#each articles as article}
+			<li>
+				<a href="/articles/{article.id}">{article.id}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
